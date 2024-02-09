020c07eec2f4be204e9304720d13b1d503743909
felix1251 <domhack123@gmail.com>
2024-02-09 14:25:04 +0800

    product card price round to 2 decimal

--- components/atoms/ProductCard.tsx
+++ components/atoms/ProductCard.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { totalPrice } from "@/utils";
+import { round, totalPrice } from "@/utils";
 import Link from "next/link";
 import React from "react";
 
@@ -21,7 +21,7 @@ const ProductCard: React.FunctionComponent<IProductCardProps> = ({
   discountPercentage,
 }: IProductCardProps) => {
   return (
-    <Link href={`/products/${id}`} className="flex flex-col gap-5 max-w-md">
+    <Link href={`/products/${id}`} className="flex flex-col gap-4 max-w-md">
       <img
         className="w-full h-52 object-cover object-center rounded-sm border"
         src={thumbnail}
@@ -31,8 +31,8 @@ const ProductCard: React.FunctionComponent<IProductCardProps> = ({
         <h3 className="font-semibold text-[15px] text-slate-800">{title}</h3>
         <span className="text-sm text-gray-500 font-bold">{brand}</span>
       </div>
-      <div className="flex gap-2 w-full justify-center font-semibold text-sm">
-        <span className="text-zinc-400">${price}</span>
+      <div className="flex gap-2 w-full justify-center font-bold text-sm">
+        <span className="text-zinc-400">${round(price, 2)}</span>
         <span className="text-secondary">
           ${totalPrice(price, discountPercentage)}
         </span>
