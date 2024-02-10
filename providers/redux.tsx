1c98e73082ea1fafc2d708f64ded62142e938d8f
felix1251 <domhack123@gmail.com>
2024-02-10 19:17:30 +0800

    whitelis & blacklist reducers

--- providers/redux.tsx
+++ providers/redux.tsx
@@ -2,13 +2,21 @@
 
 import { store } from "@/redux/store";
 import { Provider as ReduxProvider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 interface IReduxProviderProps {
   children: React.ReactNode;
 }
 
+let persistor = persistStore(store);
+
 export const Provider: React.FunctionComponent<IReduxProviderProps> = ({
   children,
 }: IReduxProviderProps) => {
-  return <ReduxProvider store={store}>{children}</ReduxProvider>;
+  return (
+    <ReduxProvider store={store}>
+      <PersistGate persistor={persistor}>{children}</PersistGate>
+    </ReduxProvider>
+  );
 };
--- redux/store.ts
+++ redux/store.ts
@@ -1,14 +1,46 @@
 "use client";
 
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import productsReducer from "./products/slice";
 import wislistReducer from "./wishlist/slice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["wishlist"],
+  blacklist: ["products"],
+};
+
+const wishlistConfig = {
+  key: "wishlist",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  wishlist: persistReducer(wishlistConfig, wislistReducer),
+  products: productsReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    products: productsReducer,
-    wishlist: wislistReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
