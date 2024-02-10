4d9b2e4b7d77784f65a8c6c9375dcd13d09de1dc
felix1251 <domhack123@gmail.com>
2024-02-10 14:43:58 +0800

    carousel nex and prev feature

--- components/atoms/Carousel.tsx
+++ components/atoms/Carousel.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useState } from "react";
 
 interface ICarousel {
@@ -10,22 +12,52 @@ interface ICarousel {
 const Carousel: React.FunctionComponent<ICarousel> = ({
   images,
 }: ICarousel) => {
-  const [displayImage, setDisplayImage] = useState<string>(images[0]);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const canNext: boolean = currentIndex + 1 < images.length;
+  const canPrev: boolean = currentIndex != 0;
+
+  const next = (): void => {
+    if (!canNext) return;
+    setCurrentIndex((state) => state + 1);
+  };
+
+  const prev = (): void => {
+    if (!canPrev) return;
+    setCurrentIndex((state) => state - 1);
+  };
 
   return (
     <div className="flex flex-col gap-6 w-[35rem]">
-      <img
-        className="h-[27rem] w-full border rouded-sm object-cover object-top"
-        src={displayImage}
-        alt="displayImage"
-      />
+      <div className="relative h-[27rem]">
+        {canNext && (
+          <button onClick={next} className="right-7 top-[45%] absolute">
+            <ArrowForwardIosIcon
+              className="text-gray-200"
+              style={{ fontSize: 50 }}
+            />
+          </button>
+        )}
+        {canPrev && (
+          <button onClick={prev} className="left-7 top-[45%] absolute">
+            <ArrowBackIosIcon
+              className="text-gray-200"
+              style={{ fontSize: 50 }}
+            />
+          </button>
+        )}
+        <img
+          className="h-full w-full border rouded-sm object-cover object-top"
+          src={images[currentIndex]}
+          alt="displayImage"
+        />
+      </div>
       <div className="flex gap-4 w-full overflow-x-auto">
-        {images.map((url) => (
-          <button key={url} onClick={() => setDisplayImage(url)}>
+        {images.map((url, index) => (
+          <button key={url} onClick={() => setCurrentIndex(index)}>
             <img
               src={url}
               className={`rounded-sm max-w-28 min-w-28 border-2 max-h-24 min-h-24 overflow-hidden object-cover object-top ${
-                displayImage === url ? "border-primary" : ""
+                images[currentIndex] === url ? "border-primary" : ""
               }`}
               alt={url}
             />
